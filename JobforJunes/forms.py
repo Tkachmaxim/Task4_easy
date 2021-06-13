e6c4adab1da50a5ddb5b63a15b660c956771c9d8
Tkachmaxim <tkachmaxim23@gmail.com>
2021-06-13 13:19:09 +0300

    fix

--- JobforJunes/forms.py
+++ JobforJunes/forms.py
@@ -2,8 +2,6 @@ from crispy_forms.helper import FormHelper
 
 from crispy_forms.layout import Layout, Row, Column, Submit
 
-from django import formsherok
-
 from JobforJunes.models import Company, Vacancy, Application, Resume
 
 
